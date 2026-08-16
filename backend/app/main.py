2de4fa4a9db758a4d515e5655f0cb0c7a8b11336
moueslati <eng.oueslati.mohamed@gmail.com>
2026-08-16 14:52:06 +0200

    Setup fastapi and add health check

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from app!")
+import logging
+from fastapi import FastAPI
+
+app = FastAPI(title="Url Shoretener")
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/health")
+async def health() -> dict[str, str]:
+    return {"status": "ok"}

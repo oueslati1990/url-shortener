73a6e476bb692f0ead5c7bdc139c13125606da4c
moueslati <eng.oueslati.mohamed@gmail.com>
2026-09-13 16:40:17 +0200

    Add prometheus to collect metrics in the backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.routers.urls import router, redirect_router
 from app.database import Base, engine
@@ -19,6 +20,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Url Shortener app", lifespan=lifespan)
+
+Instrumentator().instrument(app).expose(app)
 
 app.add_middleware(
     CORSMiddleware,
